c45af6c6d86964abffb4d4c532bf9c465e567d31
saloncopoya <151898433+saloncopoya@users.noreply.github.com>
2026-04-23 18:05:29 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,6 +4,8 @@ const urlsToCache = [
     '/index.html',
     '/offline.html',
     '/manifest.json'
+     'https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js'
 ];
 
 // Instalar Service Worker
