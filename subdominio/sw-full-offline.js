7f46d994a118a989190879059dd656be4e0e397e
saloncopoya <151898433+saloncopoya@users.noreply.github.com>
2026-05-24 15:38:04 -0600

    Update sw-full-offline.js

--- subdominio/sw-full-offline.js
+++ subdominio/sw-full-offline.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'legado-offline-v3.0.7';
-const DYNAMIC_CACHE = 'legado-dynamic-v3.0.7';
+const CACHE_NAME = 'legado-offline-v3.0.8';
+const DYNAMIC_CACHE = 'legado-dynamic-v3.0.8';
 
 
 // TODAS las URLs a cachear (incluyendo Firebase)
